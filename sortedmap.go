62af44abed89fbf9e8b792ae9c389de73ad21ec2
Justin Lowery <j@justin.sx>
2017-06-27 00:47:01 -0400

    updated comments on exported types

--- sortedmap.go
+++ sortedmap.go
@@ -1,16 +1,20 @@
 package sortedmap
 
+// SortedMap contains a map, slice, and reference to a sorting function.
+// SortedMap is not concurrency-safe, though it can be easily wrapped by a developer-defined type.
 type SortedMap struct {
 	idx    map[string]interface{}
 	sorted []string
 	lessFn SortLessFunc
 }
 
+// Record defines a type used in batching and iterations, where keys and values are used together.
 type Record struct {
 	Key string
 	Val interface{}
 }
 
+// SortLessFunc defines the type of the 'less than' comparison function for the default or chosen type.
 type SortLessFunc func(idx map[string]interface{}, sorted []string, i int, val interface{}) bool
 
 // New creates and initializes a new SortedMap structure and returns a reference to it.
